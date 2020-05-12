321e9319dc12c9105e0b138fde45dd0b3d9989f8
anshul.agarwal <anshul.agarwal@incontact.com>
2020-05-11 20:20:05 -0600

    Sample App

---
+++ GRPCLoadTest/PollingClient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrpcLoad;
+using Grpc.Core;
+using System.Threading;
+
+namespace GRPCLoadTest
+{
+    public class PollingClient
+    {
+
+        static void Main(string[] args)
+        {
+
+            Poller _poller = new Poller();
+            _poller.StartTimer();
+            Console.ReadKey();
+            
+        }
+        
+    }
+
+    public class Poller
+    {
+        public List<Timer> _queryPollTimer;
+        public Channel _grpcchannel;
+
+        public Poller()
+        {
+            _grpcchannel = new Channel("localhost", 9880, ChannelCredentials.Insecure, new List<ChannelOption>
+                                                {
+                                                    new ChannelOption(ChannelOptions.MaxReceiveMessageLength, Int32.MaxValue),
+                                                    new ChannelOption(ChannelOptions.MaxSendMessageLength, Int32.MaxValue),
+                                                    new ChannelOption(ChannelOptions.MaxConcurrentStreams,Int32.MaxValue)
+                                                });
+            _queryPollTimer = new List<Timer>();
+
+        }
+
+        public void StartTimer()
+        {
+            for (int i = 0; i < 10; i++)
+                _queryPollTimer.Add(new Timer(PollTimerCallback, this, 2000, 2000));
+        }
+
+        private async void PollTimerCallback(object timerObject)
+        {
+            try
+            {
+                List<Task> pollTasks = new List<Task>();
+                foreach (var entry in _queryPollTimer)
+                {
+                    
+                    pollTasks.Add(RequestAsync().ContinueWith(task =>
+                    {
+                        Reply result = task.Result;
+                        if (task.Exception != null)
+                            Console.WriteLine("Result Status :{0}", task.Exception);
+                        else
+                            Console.WriteLine("Result Status:{0}", "Success");
+                        
+                    }));
+
+                }
+                await Task.WhenAll(pollTasks);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Polling Client PollTimerCallback threw exception.", ex);
+            }
+        }
+
+        private async Task<Reply> RequestAsync()
+        {
+            var _client = new GrpcMessage.GrpcMessageClient(_grpcchannel);
+            return await _client.GetDataAsync(new Request(),null, DateTime.UtcNow.AddMilliseconds(3000));
+        }
+    }
+}
---
+++ GrpcServer/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrpcLoad;
+using Grpc.Core;
+using System.Threading;
+using Newtonsoft.Json;
+
+
+
+namespace GrpcServer
+{
+    class GrpcServer
+    {
+        static void Main(string[] args)
+        {
+            var _server = new Server(new List<ChannelOption>
+            {
+                new ChannelOption(ChannelOptions.MaxReceiveMessageLength, Int32.MaxValue),
+                new ChannelOption(ChannelOptions.MaxSendMessageLength, Int32.MaxValue),
+                new ChannelOption(ChannelOptions.MaxConcurrentStreams,Int32.MaxValue)
+            });
+            _server.Services.Add(GrpcMessage.BindService(new GRPCServerImpl(new int[] { 2000, 5000, 10000 })));
+            _server.Ports.Add(new ServerPort("0.0.0.0", 9880, ServerCredentials.Insecure));
+            _server.Start();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class GRPCServerImpl : GrpcMessage.GrpcMessageBase
+    {
+        public GRPCServerImpl()
+        {
+        }
+        int[] _delayCount;
+        int delay=0;
+        public GRPCServerImpl(int[] delayCounts)
+        {
+            _delayCount = delayCounts;
+        }
+        public override Task<Reply> GetData(Request request, ServerCallContext context)
+        {
+            
+                if (_delayCount != null && _delayCount.Length > 0)
+                {
+                    var sleep = 0;
+                    lock (_delayCount)
+                    {
+                        sleep = _delayCount[delay++ % _delayCount.Length];
+                    }
+                    Thread.Sleep(sleep);//cause timeout
+                }
+            Console.WriteLine("Server Responding");
+            return Task.FromResult(new Reply { Payload = new string('*', 5000000)});
+        }
+    }
+}
